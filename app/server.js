8b293d7604c6fd93b169dfd22a6c60b0eff1df4d
shanekia <jshanekia@gmail.com>
2019-08-17 22:33:56 -0400

    updated server.js file

--- app/server.js
+++ app/server.js
@@ -2,11 +2,19 @@
 
 var express =require("express");
 var path = require("path");
-
+//var parseBod =  require("body-parser");
 var app = express();
 
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 8080;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+//api and html routs
+require("./routing/apiRoutes.js")(app);
+require("./routing/htmlRoutes.js")(app);
+
+app.listen(PORT,function(){
+    console.log("App listening on PORT: " + PORT);
+});
